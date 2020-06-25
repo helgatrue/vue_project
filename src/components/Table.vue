d4f4d566bed70a061e3ecb76b717d0c69468882c
Olga Trubitsyna <otrubicina@medsoft.su>
2020-06-25 23:11:41 +0300

    New commit

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -8,7 +8,7 @@
     >
       <template v-slot:top>
         <v-toolbar flat color="white">
-          <v-toolbar-title>My CRUD</v-toolbar-title>
+          <v-toolbar-title>Table</v-toolbar-title>
           <v-divider
                   class="mx-4"
                   inset
@@ -16,68 +16,29 @@
           ></v-divider>
           <v-spacer></v-spacer>
           <v-dialog v-model="dialog" max-width="500px">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                      color="primary"
-                      dark
-                      class="mb-2"
-                      v-bind="attrs"
-                      v-on="on"
-              >New Item</v-btn>
-            </template>
+
             <v-card>
               <v-card-title>
                 <span class="headline">{{ formTitle }}</span>
               </v-card-title>
 
-              <v-card-text>
-                <v-container>
-                  <v-row>
-<!--                    <v-col cols="12" sm="6" md="4">-->
-<!--                      <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>-->
-<!--                    </v-col>-->
-<!--                    <v-col cols="12" sm="6" md="4">-->
-<!--                      <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>-->
-<!--                    </v-col>-->
-<!--                    <v-col cols="12" sm="6" md="4">-->
-<!--                      <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>-->
-<!--                    </v-col>-->
-<!--                    <v-col cols="12" sm="6" md="4">-->
-<!--                      <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>-->
-<!--                    </v-col>-->
-<!--                    <v-col cols="12" sm="6" md="4">-->
-<!--                      <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>-->
-<!--                    </v-col>-->
-                  </v-row>
-                </v-container>
-              </v-card-text>
-
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="green darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="green darken-1" text @click="save">Save</v-btn>
+                <v-btn color="green darken-1" text @click="save">Ок</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
         </v-toolbar>
       </template>
       <template v-slot:item.actions="{ item }">
-        <v-icon
-                small
-                class="mr-2"
+        <v-btn
+                color="green darken-1"
+                dark
+                class="mb-2"
+                v-bind="attrs"
+                v-on="on"
                 @click="editItem(item)"
-        >
-          mdi-pencil
-        </v-icon>
-<!--        <v-icon-->
-<!--                small-->
-<!--                @click="deleteItem(item)"-->
-<!--        >-->
-<!--          mdi-delete-->
-<!--        </v-icon>-->
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize">Reset</v-btn>
+        >More</v-btn>
       </template>
     </v-data-table>
   </v-container>
@@ -120,7 +81,7 @@
 
     computed: {
       formTitle () {
-        return this.editedIndex === -1 ? 'Сообщение об ошибке' : 'Edit Item'
+        return this.editedIndex === -1 ? 'Сообщение об ошибке' : 'Сообщение об ошибке'
       },
     },
 
@@ -155,62 +116,6 @@
             carbs: 37,
             protein: 4.3,
           },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-          },
         ]
       },
 
@@ -218,19 +123,6 @@
         this.editedIndex = this.desserts.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.dialog = true
-      },
-
-      deleteItem (item) {
-        const index = this.desserts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
-      },
-
-      close () {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
       },
 
       save () {
